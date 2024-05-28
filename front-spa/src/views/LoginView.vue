d4c297af08d3a059a55461a8e95de0ec315b52ba
atlomak <andrzej.tlomak01@gmail.com>
2024-05-28 17:42:25 +0200

    refactor(front): change endpoint to updated one

--- front-spa/src/views/LoginView.vue
+++ front-spa/src/views/LoginView.vue
@@ -30,7 +30,7 @@ export default {
   methods: {
     async login() {
       try {
-        const response = await axios.post('/api/login', {
+        const response = await axios.post('/api/auth/login', {
           username: this.username,
           password: this.password,
         });
